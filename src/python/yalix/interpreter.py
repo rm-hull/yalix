31091091b0d9216ad6d3d3bf799eb1f331d06a67
Richard Hull <rm_hull@yahoo.co.uk>
2014-06-16 09:13:33 +0100

    Parens

--- src/python/yalix/interpreter.py
+++ src/python/yalix/interpreter.py
@@ -319,9 +319,9 @@ Call(Symbol('random')).eval(env)
 
 # (let (rnd (random))
 #   (cond
-#     (< rnd 0.5)  "Unlucky"
-#     (< rnd 0.75) "Close, but no cigar"
-#     #t           "Lucky"))
+#     ((< rnd 0.5)  "Unlucky")
+#     ((< rnd 0.75) "Close, but no cigar")
+#     (#t           "Lucky")))
 #
 Let('rnd', Call(Symbol('random')),
     Cond(
