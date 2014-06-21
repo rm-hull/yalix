25a0b7a18579bbcfab83b5d9298026a5e5f4ebd8
Richard Hull <rm_hull@yahoo.co.uk>
2014-06-21 23:56:09 +0100

    Wrong indent level

--- src/python/yalix/exceptions.py
+++ src/python/yalix/exceptions.py
@@ -4,14 +4,14 @@
 from abc import ABCMeta, abstractmethod
 
 class YalixError(StandardError):
-     """ Base Yalix Exception class """
+    """ Base Yalix Exception class """
 
-     __metaclass__ = ABCMeta
+    __metaclass__ = ABCMeta
 
-     def __init__(self, message, *args):
+    def __init__(self, message, *args):
         self.value = message.format(*args)
 
-     def __str__(self):
+    def __str__(self):
         return self.value
 
 class EvaluationError(YalixError):
