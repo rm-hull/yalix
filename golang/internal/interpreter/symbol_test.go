3e54ae36e365407494fac309c6a79097e39987f5
Richard Hull <rm_hull@yahoo.co.uk>
2022-08-20 23:38:09 +0100

    Add symbol apply test

--- golang/internal/interpreter/symbol_test.go
+++ golang/internal/interpreter/symbol_test.go
@@ -10,13 +10,18 @@ import (
 
 func Test_Symbol(t *testing.T) {
 	env := environment.MakeEnv().Extend("fred", 45)
+	symbol := Symbol("fred")
 
 	t.Run("Eval", func(t *testing.T) {
-		symbol := Symbol("fred")
 		result, err := symbol.Eval(env)
 		require.Nil(t, err)
 		require.Equal(t, 45, result)
 		require.Equal(t, "fred", symbol.Repr())
+	})
+
+	t.Run("Apply", func(t *testing.T) {
+		_, err := symbol.Apply(env, MakeCaller(symbol))
+		require.EqualError(t, err, "cannot invoke with: 'fred'")
 	})
 
 	t.Run("Quoted", func(t *testing.T) {
