e4d07c3d8feadce78569e8b2554fd853cebaeaba
Richard Hull <rm_hull@yahoo.co.uk>
2022-08-21 12:22:06 +0100

    Use %T format verb

--- golang/internal/util/converter.go
+++ golang/internal/util/converter.go
@@ -1,15 +1,13 @@
 package util
 
 import (
-	"reflect"
-
 	"github.com/pkg/errors"
 )
 
 func CastAs[T any](value any) (T, error) {
 	tValue, ok := value.(T)
 	if !ok {
-		return tValue, errors.Errorf("cannot convert '%v' (%s) to %s", value, reflect.TypeOf(value), reflect.TypeOf(tValue))
+		return tValue, errors.Errorf("cannot convert '%v' (%T) to %T", value, value, tValue)
 	}
 	return tValue, nil
 }
